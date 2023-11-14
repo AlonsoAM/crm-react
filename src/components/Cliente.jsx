b0b3e4ab697598511258918beb191701db581b7a
Alonso Anchante Moreno <alonso.amoreno@live.com>
2023-11-14 09:55:20 -0500

    Terminando la tabla de clientes

--- src/components/Cliente.jsx
+++ src/components/Cliente.jsx
@@ -1,10 +1,29 @@
 export const Cliente = ({ cliente }) => {
   const { id, nombre, telefono, empresa, email } = cliente;
   return (
-    <tr key={id} className="hover">
-      <td>{nombre}</td>
-      <td>{telefono}</td>
-      <td></td>
+    <tr key={id}>
+      <td className="space-y-2 ">
+        {" "}
+        <p className="text-2xl text-gray-800 ">{nombre}</p> <p>{empresa}</p>
+      </td>
+      <td>
+        <p className="text-gray-600 ">
+          <span className="text-gray-800 font-bold uppercase">Email: </span>{" "}
+          {email}
+        </p>
+        <p className="text-gray-600 ">
+          <span className="text-gray-800 font-bold uppercase">Teléfono: </span>{" "}
+          {telefono}
+        </p>
+      </td>
+      <td className="flex gap-3">
+        <button className="btn btn-xs btn-ghost font-bold text-blue-600 uppercase hover:text-blue-800">
+          Editar
+        </button>
+        <button className="btn btn-xs btn-ghost font-bold text-red-600 uppercase hover:text-red-800">
+          Eliminar
+        </button>
+      </td>
     </tr>
   );
 };
